0cb6844d1641aec4cb2c3b03a4e6d9321b326428
Gustavo Sapienza <gustavo.sapienza@agaxtur.com.br>
2019-09-25 16:45:59 -0300

    Inicial

---
+++ orkutaine.js
@@ -0,0 +1,24 @@
+
+function onWindowLoad() {
+	document.body.innerHTML = document.body.innerHTML.replace(/Bolsonaro/g, 'Bolsonaro retardado');
+
+	var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+	link.type = 'image/x-icon';
+	link.rel = 'shortcut icon';
+	link.href = 'http://www.stackoverflow.com/favicon.ico';
+	document.getElementsByTagName('head')[0].appendChild(link);
+
+
+
+	if(document.querySelector(".focus_target form a img").src && document.querySelector(".imgWrap > img").src){
+		document.querySelector(".imgWrap > img").src = document.querySelector(".focus_target form a img").src;
+	}
+}
+
+document.getElementsByTagName("body").onclick = function(e){
+   setTimeout(function(){ 
+   	onWindowLoad()
+   }, 1000);
+}
+
+window.onload = onWindowLoad;
